3b03583e4dadecf3b038f3cb60b200b590401886
MrShadowsWolf <loup.thevenin.1@gmail.com>
2024-09-30 19:46:14 +0200

    add clang-format

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -11,6 +11,7 @@ return {
 				"shfmt",
 				"css-lsp",
                 "clangd",
+				"clang-format",
                 "python-lsp-server",
                 "bash-language-server",
                 "cmake-language-server",
@@ -175,5 +176,52 @@ return {
 			setup = {},
 		},
 	},
+	-- Norminette format with clang formt + norminette diagnostics
+	-- {
+	-- 	"nvimtools/none-ls.nvim",
+	-- 	event = "LazyFile",
+	-- 	dependencies = { "mason.nvim" },
+	-- 	init = function()
+	-- 	  LazyVim.on_very_lazy(function()
+	-- 		-- register the formatter with LazyVim
+	-- 		LazyVim.format.register({
+	-- 		  name = "none-ls.nvim",
+	-- 		  priority = 200, -- set higher than conform, the builtin formatter
+	-- 		  primary = true,
+	-- 		  format = function(buf)
+	-- 			return LazyVim.lsp.format({
+	-- 			  bufnr = buf,
+	-- 			  filter = function(client)
+	-- 				return client.name == "null-ls"
+	-- 			  end,
+	-- 			})
+	-- 		  end,
+	-- 		  sources = function(buf)
+	-- 			local ret = require("null-ls.sources").get_available(vim.bo[buf].filetype, "NULL_LS_FORMATTING") or {}
+	-- 			return vim.tbl_map(function(source)
+	-- 			  return source.name
+	-- 			end, ret)
+	-- 		  end,
+	-- 		})
+	-- 	  end)
+	-- 	end,
+	-- 	opts = function(_, opts)
+	-- 	  local nls = require("null-ls")
+	-- 	  opts.root_dir = opts.root_dir
+	-- 		or require("null-ls.utils").root_pattern(".null-ls-root", ".neoconf.json", "Makefile", ".git")
+	-- 	  opts.sources = vim.list_extend(opts.sources or {}, {
+	-- 		nls.builtins.formatting.fish_indent,
+	-- 		nls.builtins.diagnostics.fish,
+	-- 		nls.builtins.diagnostics.norminette.with({
+	-- 			command = "norminette",
+	-- 		}),
+	-- 		nls.builtins.formatting.stylua,
+	-- 		nls.builtins.formatting.shfmt,
+	-- 		nls.builtins.formatting.clang_format.with({
+	-- 			extra_args = { "--assume-filename=" .. vim.fn.expand("~/.config/nvim/lua/craft/.clang-format") },
+	-- 		}),
+	-- 	  })
+	-- 	end,
+	--   },
 
 }
