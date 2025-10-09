a5c2652368b6bd82b3378ce33a603aef4903ea23
Loupthevenin <loup.thevenin.1@gmail.com>
2025-10-09 14:14:42 +0200

    no background

--- .config/wezterm/constants.lua
+++ .config/wezterm/constants.lua
@@ -4,6 +4,6 @@ M.bg_blurred = os.getenv("HOME") .. "/.config/wezterm/assets/bg_blurred.jpg"
 M.bg_image_4 = os.getenv("HOME") .. "/.config/wezterm/assets/image_4.jpg"
 M.bg_monolith = os.getenv("HOME") .. "/.config/wezterm/assets/monolith.jpg"
 
-M.bg_image = M.bg_blurred
+M.bg_image = ""
 
 return M
